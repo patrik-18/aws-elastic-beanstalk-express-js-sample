cf8b6ac69af108ded8dbba4c0b66f578639486c3
Kristel <patrik.kristel1@dxc.com>
2021-02-02 15:45:10 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Patrik is the best. not allowed access.'));
+app.get('/', (req, res) => res.send('Hello World! Patrik is the best. not allowed access. Another one.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
